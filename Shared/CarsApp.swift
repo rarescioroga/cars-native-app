e49987da19dc97c270fa9c357667ec7d48b100d7
Rares Cioroga <ciorogarares@gmail.com>
2021-12-05 12:25:27 +0200

    Initial Commit

---
+++ Shared/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Shared
+//
+//  Created by user on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
